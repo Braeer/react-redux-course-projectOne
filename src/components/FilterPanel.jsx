e61810d1ebddb7cfcbb0082b3e6f60d58546c93f
Timur <114247583+Braeer@users.noreply.github.com>
2025-04-09 15:25:34 +0300

    ADD: Dockerfile UPDATE: components

--- src/components/FilterPanel.jsx
+++ src/components/FilterPanel.jsx
@@ -5,24 +5,27 @@ import { Card } from 'UI/Card';
 import { Stack } from 'UI/Stack';
 
 import { selectFilters } from 'store/filters/filter-selectors';
+import { removeFilter, clearFilter } from 'store/filters/filter-actions';
 
 const FilterPanel = () => {
+  const dispatch = useDispatch();
   const currentFilters = useSelector(selectFilters);
+
+  if (currentFilters.length === 0) return null;
   return (
     <Card className="filter-panel">
       <div className="filter-panel-wrapper">
         <Stack>
           {currentFilters.map((filter) => (
-            <Badge key={filter} variant="clearable">
+            <Badge key={filter} variant="clearable" onClear={() => dispatch(removeFilter(filter))}>
               {filter}
             </Badge>
           ))}
-          <Badge variant="clearable">Frontend</Badge>
-          <Badge variant="clearable">Backend</Badge>
-          <Badge variant="clearable">React</Badge>
         </Stack>
 
-        <button className="link">Clear</button>
+        <button onClick={() => dispatch(clearFilter)} className="link">
+          Clear
+        </button>
       </div>
     </Card>
   );
